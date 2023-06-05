fc2cf6d6530ec1be9699148e175bd89a82f2a7b9
zaifer <zaif3r@pm.me>
2023-06-05 01:40:02 -0500

    fix typo

--- src/api/ichart-api.ts
+++ src/api/ichart-api.ts
@@ -217,7 +217,7 @@ export interface IChartApi {
 	 */
 	subscribeCrosshairMove(handler: MouseEventHandler): void;
 
-	setCrossHairXY(x: number,y: number,visible: boolean): void;
+	setCrosshairXY(x: number, y: number, visible: boolean): void;
 	/**
 	 * Unsubscribe a handler that was previously subscribed using {@link subscribeCrosshairMove}.
 	 *
